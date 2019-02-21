b66053e54123b14fcf99dd37f3d4ce7123a8f0e4
SALMAN <elfadili.salman@gmail.com>
2019-02-21 16:34:18 +0100

    Finish scrollSpy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/* ScrollSpy service */
+$(window).bind('scroll', function() {
+    var currentTop = $(window).scrollTop();
+    var halfWindow = $(window).height() / 2;
+    var changePoint = currentTop + halfWindow;
+    var elems = $('.scrollspy');
+    elems.each(function(index){
+      var elemTop 	    = $(this).offset().top;
+      var elemBottom 	= elemTop + $(this).height();
+      if(changePoint >= elemTop && changePoint <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+        navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
+}); 
 
 /* Smooth-scroll service */
 $("a[href^='#']").click(function(e) {
